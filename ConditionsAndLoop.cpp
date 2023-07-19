a662d19f62aa4448aa11238b8da86d0cea32d157
Sunil <sunil31725@gmail.com>
2023-07-19 23:14:21 +0530

    Conditions And Loop

---
+++ ConditionsAndLoop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// check a number is positive or negative
+
+int main () {
+    int a;
+    cin >> a;
+    if(a > 0){
+        cout << a  << " is positive number" << endl;
+    } else{
+         cout << a  << " is negative number" << endl;
+    } 
+}
