bf75e00ad5a34267ad14915470577a5e86046d82
Sunil <sunil31725@gmail.com>
2023-07-19 23:49:54 +0530

    Conditions And Loop

--- ConditionsAndLoop.cpp
+++ ConditionsAndLoop.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-// check a number is positive or negative
+// 1. check a number is positive or negative
 
 int main () {
     int a;
@@ -11,4 +11,23 @@ int main () {
     } else{
          cout << a  << " is negative number" << endl;
     } 
+}
+
+#include <iostream>
+using namespace std;
+
+// 2. check whether the given character is in upper case, lower case, or non-alphabetic character 
+
+int main () {
+    char ch;
+    cout << "Enter the Value if ch : ";
+    cin >> ch;
+  if (ch >= 'A' && ch <= 'Z')
+        cout << ch << " is an UpperCase character\n";
+ 
+    else if (ch >= 'a' && ch <= 'z')
+        cout << ch << " is an LowerCase character\n";
+ 
+    else
+        cout << ch << " is not an alphabetic character\n";
 }
