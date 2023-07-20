409833680af878179a7af3a4505049cc2cb9633e
Sunil <sunil31725@gmail.com>
2023-07-20 23:50:04 +0530

    patterns.cpp

--- patterns.cpp
+++ patterns.cpp
@@ -93,3 +93,24 @@ int main() {
         i++;
     }
 }
+
+// 5. Print numbers in left triangle Pattern
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows;
+    cout << "Enter the rows ";
+    cin >> rows;
+    int i = 1;
+    while ( i <= rows )
+    {
+        int j = 1;
+        while ( j <= i) {
+            cout << i << " ";
+            j ++;
+        }
+        cout << endl;
+        i++;
+    }
+}
