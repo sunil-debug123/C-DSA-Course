0776c2f37ff688722166821a319bdf6e9e3ff5e2
Sunil <sunil31725@gmail.com>
2023-07-25 00:05:30 +0530

    forloop

---
+++ forloop.cpp
@@ -0,0 +1,13 @@
+// 1. Print  count from 1 to N
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 1;
+    cout << "Enter the number : ";
+    cin >> n;
+    for (int i =1; i <=n ; i++){
+        cout << i << endl;
+    }
+}
