c5aa550a6a925eee8bdd085ff0dbd5b3520fb457
Sunil <sunil31725@gmail.com>
2023-07-24 22:30:18 +0530

    patterns

--- patterns.cpp
+++ patterns.cpp
@@ -546,3 +546,30 @@ int main() {
        i++;
    }
 }
+
+// 24 .Print mirror number triangle;
+#include <iostream>
+using namespace std;
+
+int main() {
+   int rows;
+   cout << "Enter number of Rows : ";
+   cin >> rows;
+   int i = 1;
+   while ( i <= rows)
+   {
+       int j = i;
+       int k = 1;
+       while (j < rows)
+       {
+           cout << "_" << " ";
+           j++;
+       }
+       while (k<= i ){
+           cout << i << " ";
+           k++;
+       }
+       cout << endl;
+       i++;
+   }
+}
