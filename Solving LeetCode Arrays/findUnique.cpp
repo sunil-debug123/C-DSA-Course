ed8682d36684da9c0d5b4a4ba914230f75ab66f1
Sunil <sunil31725@gmail.com>
2023-07-31 00:50:05 +0530

    LeetCode Problems

--- Solving LeetCode Arrays/findUnique.cpp
+++ Solving LeetCode Arrays/findUnique.cpp
@@ -1,9 +1,26 @@
-int findUnique(int *arr, int size)
+#include <iostream>
+using namespace std;
+
+
+int main()
 {
-   	int ans = 0;
+    int array1[] = { 8, 6, 8, 2, 1};
+    int s1 = sizeof(array1)/sizeof(array1[0]);
+
+    cout << "Original array: ";
     
-    for(int i = 0; i<size; i++) {
-        ans = ans^arr[i];
+    for (int i=0; i < s1; i++) 
+    cout << array1[i] <<" ";
+    
+    cout <<"\nUnique elements of the said array: ";
+    for (int i=0; i<s1; i++)
+    {
+        int j;
+        for (j=0; j<i; j++)
+           if (array1[i] == array1[j])
+               break;
+         if (i == j)
+          cout << array1[i] << " ";
     }
-    return ans;
+    return 0; 
 }
