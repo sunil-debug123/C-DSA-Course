7e415be3abd506d9b1d8c441ddc7ad8acf3a0fcc
Sunil <sunil31725@gmail.com>
2023-08-03 21:49:00 +0530

    LeetCode Problems

--- Solving LeetCode Arrays/findDuplicatesInArray.cpp
+++ Solving LeetCode Arrays/findDuplicatesInArray.cpp
@@ -1,30 +1,37 @@
 #include <iostream>
-#include <math.h>
+#include <vector>
 using namespace std;
 
-void findDuplicates(int arr[], int size){
-    for(int i = 0; i < size; i++){
-        int j;
-      for(j=0; j < i; j++){
-          if (arr[i] == arr[j]){
-              arr[i] = arr[j];
-              cout << arr[j];
-         }
-      }
+void findDuplicates(int arr[], int size, vector<int>& duplicates) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < i; j++) {
+            if (arr[i] == arr[j]) {
+                duplicates.push_back(arr[i]);
+                break; // Once a duplicate is found, no need to check further
+            }
+        }
     }
 }
 
 int main() {
     int n; 
-    cout << "Enter the Size of Array : ";
+    cout << "Enter the Size of Array: ";
     cin >> n;
     int array[n]; 
     
-    cout << "Enter the Array Elements : ";
-    for(int i = 0; i < n ; i++){
+    cout << "Enter the Array Elements: ";
+    for (int i = 0; i < n; i++) {
         cin >> array[i];
     }
-    findDuplicates(array, n);
+    
+    vector<int> duplicates;
+
+    findDuplicates(array, n, duplicates);
+
+    cout << "Duplicate Elements: ";
+    for (int i = 0; i < duplicates.size(); i++) {
+        cout << duplicates[i] << " ";
+    }
    
     return 0;
 }
