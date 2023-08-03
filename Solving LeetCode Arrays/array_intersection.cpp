ec28dfdb3baf3524f49e24813c069c4fab382dbe
Sunil <sunil31725@gmail.com>
2023-08-03 22:38:15 +0530

    LeetCode Problems

--- Solving LeetCode Arrays/array_intersection.cpp
+++ Solving LeetCode Arrays/array_intersection.cpp
@@ -1,25 +1,58 @@
-#include<vector>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
-vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, int m)
-{
-	int i = 0, j = 0;
-    vector<int> ans;
-    while(i<n && j<m) {
+
+void findInterSection(int arr1[], int arr2[], int size1, int size2, vector<int>& duplicates) {
+    sort(arr1, arr1 + size1);
+    sort(arr2, arr2 + size2);
+
+    int i = 0, j = 0;
+    while (i < size1 && j < size2) {
+        if (arr1[i] == arr2[j]) {
+            duplicates.push_back(arr1[i]);
+            i++;
+            j++;
+        } else if (arr1[i] < arr2[j]) {
+            i++;
+        } else {
+            j++;
+        }
+    }
+}
+
+int main() {
+    int n1; 
+    cout << "Enter the Size of Array 1: ";
+    cin >> n1;
+    int array1[n1]; 
     
-    if(arr1[i]==arr2[j])
-    {
-    	ans.push_back(arr1[i]);
-        i++;
-        j++;
-    }
-    else if (arr1[i] < arr2[j]) {
-  		i++;
-    }
-    else
-    {
-    	j++;
+    cout << "Enter the Array 1 Elements: ";
+    for (int i = 0; i < n1; i++) {
+        cin >> array1[i];
     }
     
+    int n2;
+    cout << "Enter size of Array 2: ";
+    cin >> n2;
+    int array2[n2];
+    cout << "Enter the Array 2 Elements: ";
+    for (int i = 0; i < n2; i++) {
+        cin >> array2[i];
     }
-    return ans;
+    
+    vector<int> duplicates;
+
+    findInterSection(array1, array2, n1, n2, duplicates);
+    
+    if (duplicates.empty()) {
+        cout << "-1";  // No intersection
+    } else {
+        cout << "Duplicate Elements: ";
+        for (int i = 0; i < duplicates.size(); i++) {
+            cout << duplicates[i] << " ";
+        }
+    }
+    
+    return 0;
 }
